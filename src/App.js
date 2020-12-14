4e0ae378947527d7fabaeba069f423a87ac52ffb
gftherd <gftherd.p@gmail.com>
2020-12-14 15:11:24 +0700

    update login facebook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from '@dotmatter/reaction-plugin-facebook-login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <FacebookLogin
+          appId="291077344314809"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+
+        />
+
       </header>
     </div>
   );
